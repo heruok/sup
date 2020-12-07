e7f383d645c2077f46bfb6d37fdd0449c0dabcac
heruok <75049821+heruok@users.noreply.github.com>
2020-12-07 21:00:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 const Sup = require('./commands/sup')
 const Prout = require('./commands/prout')
-const Gay = require('./commands/gay')
+const Alex = require('./commands/alex')
 
 
 bot.on('message', function (message) {
     let commandUsed =
         Sup.parse(message)
         Prout.parse(message)
+        Alex.parse(message)
 })
 
 bot.login(process.env.TOKEN)
