a081bd53a2380fbc724a2ad86186731fc2d3a653
heruok <75049821+heruok@users.noreply.github.com>
2020-12-07 20:50:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 const Sup = require('./commands/sup')
 const Prout = require('./commands/prout')
+const Gay = require('./commands/gay')
 
 
 bot.on('message', function (message) {
