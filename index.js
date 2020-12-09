c24775bdc0b048e91eeec8b9665fdb4071995d9c
heruok <75049821+heruok@users.noreply.github.com>
2020-12-09 23:41:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Sup = require('./commands/sup')
 const Prout = require('./commands/prout')
 const Alex = require('./commands/alex')
 const Paf = require('./commands/paf')
+const Shi = require('./commands/shi')
 
 
 bot.on('message', function (message) {
@@ -12,6 +13,7 @@ bot.on('message', function (message) {
         Prout.parse(message)
         Alex.parse(message)
         Paf.parse(message)
+        Shi.parse(message)
 })
 
 bot.login(process.env.TOKEN)
